0fe1c4cd71aa3b283c2412bbedce8e0cd23b0dfe
Frederic Crozat <fcrozat@suse.com>
2021-12-03 18:12:11 +0100

    ensure distribution-release is installed

--- build-nginx-scratch.sh
+++ build-nginx-scratch.sh
@@ -8,8 +8,8 @@ scratchmnt=$(buildah mount $newcontainer)
 
 # install the packages
 zypper --root $scratchmnt ar 'https://updates.suse.com/SUSE/Products/SLE-BCI/$releasever_major-SP$releasever_minor/$basearch/product/' SLE_BCI
-zypper --root $scratchmnt --non-interactive --releasever 15.3 --gpg-auto-import-keys in --no-recommends nginx
-rm -rf $scratchmnt/var/cache/rpms
+zypper --root $scratchmnt --non-interactive --releasever 15.3 --gpg-auto-import-keys in --no-recommends distribution-release nginx
+zypper --root $scratchmnt clean
 
 # set some config info
 buildah config --label name=nginx-bci-scratch $newcontainer
