90fb2e1a9bd897e41c0a30405cd0716b51fb6794
Frederic Crozat <fcrozat@suse.com>
2021-12-07 17:41:41 +0100

    reduce size even more

--- build-nginx-micro.sh
+++ build-nginx-micro.sh
@@ -8,8 +8,10 @@ scratchmnt=$(buildah mount $newcontainer)
 
 # install the packages
 zypper --root $scratchmnt ar 'https://updates.suse.com/SUSE/Products/SLE-BCI/$releasever_major-SP$releasever_minor/$basearch/product/' SLE_BCI
+export ZYPP_CONF=$PWD/scratch-zypp.conf
+zypper --root $scratchmnt al cracklib-dict-full
 zypper --root $scratchmnt --non-interactive --releasever 15.3 --gpg-auto-import-keys in --no-recommends nginx
-rm -rf $scratchmnt/var/cache/rpms
+zypper --root $scratchmnt --non-interactive clean -a
 
 # set some config info
 buildah config --label name=nginx-bci-scratch $newcontainer
